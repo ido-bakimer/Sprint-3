50fc526aacb311dcf098520f4dc7067bfbcfb821
idob <idobakimer@gmail.com>
2022-04-30 21:56:22 +0300

    day 4 21:55 ido

--- cmps/email-cmps/email-preview.jsx
+++ cmps/email-cmps/email-preview.jsx
@@ -8,11 +8,13 @@ export function EmailPreview({ email, onPreviewClick, onRemove, onToggleStar,onT
     const month = date.toLocaleString('en-us', { month: 'short' })
     const year = date.getFullYear()
     return <div
-        onClick={() => onPreviewClick(email)} className={'email-preview flex space-between ' + (email.isRead ? 'read' : '')}>
+     onClick={() => onPreviewClick(email)} className={'email-preview flex space-between ' + (email.isRead ? 'read' : '')}>
+        <div className="mail-prev-container">
         <div className="from">{email.status === 'inbox' ? `${email.from}(${email.status})` : `${email.to}(${email.status})`}</div>
         <div className="subject">{email.subject}</div>
-        <EmailBtns onRemove={onRemove} onToggleStar={onToggleStar} onToggleRead={onToggleRead} onRecycle={onRecycle} email={email} makeNotefromEmail={makeNotefromEmail} />
         <div className="date">{date.getDate()}/{month}/{year}</div>
+        </div>
+        <EmailBtns onRemove={onRemove} onToggleStar={onToggleStar} onToggleRead={onToggleRead} onRecycle={onRecycle} email={email} makeNotefromEmail={makeNotefromEmail} />
     </div>
 
 }
