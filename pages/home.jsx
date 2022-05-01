c3420b483dfc28416abdd032d0e779e89ae7658c
idob <idobakimer@gmail.com>
2022-05-01 20:44:32 +0300

    another

--- pages/home.jsx
+++ pages/home.jsx
@@ -11,7 +11,7 @@ export function Home() {
         <h2 className="animate__animated animate__fadeInRight">Welcome to YAWHO?</h2>
         <p>If you ever wanted to find <span>EVERY BOOK THAT EVER EXISTED ONLINE</span>, or <span>STORE YOUR VERY OWN PERSONILISED NOTES</span> and even <span>SENDING AND RECIEVEING* MAILS, NO PASSWORD OR ACOUNT NEEDED</span>, then you have come to the right place! Here at YAWHO™ we pride ourselved with cutting edge react technology, and we hope you may find some use of our vast application list.</p>
     
-        <div className="mount"><img className="mountain-container" src={'https://i.ibb.co/bPjTSps/mountains.png'} alt="" /></div>
+        <div className="mount"><img className="mountain-container" src='https://i.ibb.co/bPjTSps/mountains.png' alt="" /></div>
         
 
     </section>
